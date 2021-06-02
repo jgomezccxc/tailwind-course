f48838fe427c11fc5876cc45e9c6e794f58a1c9f
Javier Gómez <javiergomezve@gmail.com>
2021-06-02 09:57:57 -0500

    Install tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { Fragment } from 'react';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <Fragment>
+            <Head>
+                <meta charset="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, user-scalable=no"
+                />
+                <title>Curso Tailwind</title>
+            </Head>
+            <body>
+                <Component {...pageProps} />
+            </body>
+        </Fragment>
+    );
 }
 
-export default MyApp
+export default MyApp;
